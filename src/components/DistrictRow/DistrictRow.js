84cefcba185c7d7f03ea42603b5aa1fa6414aab7
Susan Subedi <ssubedi1@go.olemiss.edu>
2020-05-17 18:19:59 -0500

    chart functionality done succesfully

--- src/components/DistrictRow/DistrictRow.js
+++ src/components/DistrictRow/DistrictRow.js
@@ -1,40 +1,11 @@
-import React, { useState, useEffect } from "react";
-import axios from "../../axios";
+import React from "react";
 
-const DistrictRow = ({ name, id }) => {
-  const [cases, setCases] = useState({
-    total: 0,
-    active: 0,
-    deaths: 0,
-    recovered: 0,
-  });
-
-  useEffect(() => {
-    const getDistrict = async (id) => {
-      try {
-        const {
-          data: { total, activeCase, recoveredCase, deathCase },
-        } = await axios.get(`/${id}`);
-
-        setCases({
-          total,
-          active: activeCase,
-          recovered: recoveredCase,
-          death: deathCase,
-        });
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    getDistrict(id);
-  }, [id]);
-
+const DistrictRow = ({ name, ...props }) => {
   return (
     <tr>
       <td>{name}</td>
-      {Object.keys(cases)
-      .map((corona) => (
-        <td>{cases[corona]}</td>
+      {Object.keys(props).map((corona) => (
+        <td>{props[corona]}</td>
       ))}
     </tr>
   );
--- src/components/DistrictsChart/DistrictsCharts.js
+++ src/components/DistrictsChart/DistrictsCharts.js
@@ -6,43 +6,55 @@ const DistrictsCharts = () => {
     const getAllDistricts = async () => {
       try {
         const { data } = await axios.get("/");
-        console.log(data);
-        setDisricts(data);
+        setDisricts({
+          cases: data,
+          loading: false,
+        });
       } catch (error) {
         console.log(error);
       }
     };
     getAllDistricts();
   }, []);
-  const [districts, setDisricts] = useState([]);
+  const [districts, setDisricts] = useState({
+    cases: [],
+    loading: true,
+  });
+  const { cases, loading } = districts;
 
   return (
     <div>
       <h1>District Cases</h1>
-      <table
-        style={{
-          marginTop: "20px",
-          marginLeft: "auto",
-          marginRight: "auto",
-          borderSpacing: "10px",
-          borderCollapse: "separate",
-        }}
-      >
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Total</th>
-            <th>Active</th>
-            <th>Deceased</th>
-            <th>Recovered</th>
-          </tr>
-        </thead>
-        <tbody>
-          {districts.map(({ id, englishTitle }) => {
-            return <DistrictRow key={id} id={id} name={englishTitle} />;
-          })}
-        </tbody>
-      </table>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <table
+          style={{
+            marginTop: "20px",
+            marginLeft: "auto",
+            marginRight: "auto",
+            borderSpacing: "10px",
+            borderCollapse: "separate",
+          }}
+        >
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Total</th>
+              <th>Active</th>
+              <th>Recovered</th>
+              <th>Deceased</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cases
+              .sort((a, b) => a.total < b.total)
+              .map(({ id, ...rest }) => {
+                return <DistrictRow key={id} {...rest} />;
+              })}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
