f66ddd9608e2d0e59e47bdeb6ba14ce29537cb5c
sushantbaskota2 <sushantbaskota2@gmail.com>
2020-05-18 21:57:18 -0500

    Added Nav bar

--- src/components/MainContainer/MainContainer.js
+++ src/components/MainContainer/MainContainer.js
@@ -1,16 +1,18 @@
-import React from "react";
-import "./MainContainer.css";
-import Cases from "../Cases/Cases";
-import MainChart from "../MainChart/MainChart";
+import React from 'react';
+import './MainContainer.css';
+import Nav from '../Navbar/Nav';
+import Cases from '../Cases/Cases';
+import MainChart from '../MainChart/MainChart';
 const MainContainer = () => {
-  return (
-    <div className="container">
-      <div className="row">
-        <Cases />
-        <MainChart />
-      </div>
-    </div>
-  );
+    return (
+        <div className='container'>
+            <div className='row'>
+                <Nav />
+                <Cases />
+                <MainChart />
+            </div>
+        </div>
+    );
 };
 
 export default MainContainer;
