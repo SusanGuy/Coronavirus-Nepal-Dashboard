ac2e275d40aeeaf841df72b28b2c796761f80a16
Susan Subedi <ssubedi1@go.olemiss.edu>
2020-05-18 23:32:40 -0500

    Avatar component fixed

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -3,7 +3,7 @@ import React from "react";
 const Avatar = ({ src, github }) => {
   return (
     <a href={github} target="_blank">
-      <img alt="creator" class="user-avatar -large" src={src} />
+      <img alt="creator" className="user-avatar -large" src={src} />
     </a>
   );
 };
