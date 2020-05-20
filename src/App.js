b2f8e5b5c939a60d4a48e38813d9c8206e0a6928
sushantbaskota2 <sushantbaskota2@gmail.com>
2020-05-20 02:35:38 -0500

    Feed added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React, { useState } from "react";
-import Nav from "./components/Nav/Nav";
-import Home from "./containers/Home/home";
-import AboutUs from "./containers/AboutUs/AboutUs";
-import Footer from "./components/Footer/Footer";
-import "./App.scss";
-import { Switch, Route } from "react-router-dom";
+import React, { useState } from 'react';
+import Nav from './components/Nav/Nav';
+import Home from './containers/Home/home';
+import AboutUs from './containers/AboutUs/AboutUs';
+import Footer from './components/Footer/Footer';
+import './App.scss';
+import { Switch, Route } from 'react-router-dom';
+import Feed from './containers/Feed/Feed';
 function App() {
-  const [mode, setMode] = useState(false);
-  return (
-    <div className={`App ${mode ? "" : "dark-mode"}`}>
-      <Nav setMode={setMode} mode={mode} />
-      <Switch>
-        <Route path="/about-us" exact component={AboutUs} />
-        <Route path="/" component={Home} />
-      </Switch>
-      <Footer />
-    </div>
-  );
+    const [ mode, setMode ] = useState(false);
+    return (
+        <div className={`App ${mode ? '' : 'dark-mode'}`}>
+            <Nav setMode={setMode} mode={mode} />
+            <Switch>
+                <Route path='/about-us' exact component={AboutUs} />
+                <Route path='/feed' exact component={Feed} />
+                <Route path='/' component={Home} />
+            </Switch>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/containers/Feed/Feed.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Feed = () => {
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <h1>Under Construction</h1>
+        </div>
+    );
+};
+
+export default Feed;
