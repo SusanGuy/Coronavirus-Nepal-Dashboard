58e5af10caaff5950e9a460e5aaa5ee89268c979
sushantbaskota2 <sushantbaskota2@gmail.com>
2020-05-21 18:54:11 -0500

    Added Twitter Feed

--- src/containers/Feed/Feed.js
+++ src/containers/Feed/Feed.js
@@ -1,23 +1,30 @@
-import React from "react";
-import FacebookFeed from "../../components/FacebookFeed/FacebookFeed";
-
+import React from 'react';
+import { TwitterTimelineEmbed } from 'react-twitter-embed';
 const Feed = () => {
-
     return (
         <div className={'Home'} style={{ textAlign: 'center' }}>
             <div className='about-header' style={{ marginTop: '10px' }}>
                 <h1>Feed</h1>
             </div>
             <div className='home-left' style={{ paddingTop: '50px' }}>
-                <FacebookFeed link='mohpnep' />
+                <TwitterTimelineEmbed
+                    sourceType='timeline'
+                    url={'https://twitter.com/RONBupdates'}
+                    theme='dark'
+                    options={{ height: 600, width: 400 }}
+                />
             </div>
 
             <div className='home-right' style={{ paddingTop: '50px' }}>
-                <FacebookFeed link='officialroutineofnepalbanda' />
+                <TwitterTimelineEmbed
+                    sourceType='timeline'
+                    url={'https://twitter.com/mohpnep'}
+                    theme='dark'
+                    options={{ height: 600, width: 400 }}
+                />
             </div>
         </div>
     );
-
 };
 
 export default Feed;
