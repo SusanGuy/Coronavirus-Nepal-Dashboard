e3e885871d9c6aa9129c52dafc927bc54a29de4b
Susan Subedi <ssubedi1@go.olemiss.edu>
2020-05-21 01:00:23 -0500

    Fixed padding

--- src/containers/Feed/Feed.js
+++ src/containers/Feed/Feed.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import MapView from '../../components/MapView/MapView';
-import FacebookFeed from '../../components/FacebookFeed/FacebookFeed';
+import React from "react";
+import FacebookFeed from "../../components/FacebookFeed/FacebookFeed";
 
 const Feed = () => {
-    return (
-        <div className={'Home'} style={{ textAlign: 'center' }}>
-            <div className='about-header'>
-                <h1>Feed</h1>
-            </div>
-            <div className='home-left'>
-                <FacebookFeed link='mohpnep' />
-            </div>
+  return (
+    <div
+      className={"Home"}
+      style={{ marginTop: "20px", paddingTop: "0px", textAlign: "center" }}
+    >
+      <div className="about-header">
+        <h1>Feed</h1>
+      </div>
+      <div className="home-left">
+        <FacebookFeed link="mohpnep" />
+      </div>
 
-            <div className='home-right'>
-                <FacebookFeed link='officialroutineofnepalbanda' />
-            </div>
-        </div>
-    );
+      <div className="home-right">
+        <FacebookFeed link="officialroutineofnepalbanda" />
+      </div>
+    </div>
+  );
 };
 
 export default Feed;
