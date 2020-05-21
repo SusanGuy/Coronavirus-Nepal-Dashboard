950662cba58c3113be681a8a75a1f450e22e2719
sushantbaskota2 <sushantbaskota2@gmail.com>
2020-05-21 01:06:46 -0500

    Fixed feed

--- src/containers/Feed/Feed.js
+++ src/containers/Feed/Feed.js
@@ -5,14 +5,14 @@ import FacebookFeed from '../../components/FacebookFeed/FacebookFeed';
 const Feed = () => {
     return (
         <div className={'Home'} style={{ textAlign: 'center' }}>
-            <div className='about-header'>
+            <div className='about-header' style={{ marginTop: '10px' }}>
                 <h1>Feed</h1>
             </div>
-            <div className='home-left'>
+            <div className='home-left' style={{ paddingTop: '50px' }}>
                 <FacebookFeed link='mohpnep' />
             </div>
 
-            <div className='home-right'>
+            <div className='home-right' style={{ paddingTop: '50px' }}>
                 <FacebookFeed link='officialroutineofnepalbanda' />
             </div>
         </div>
