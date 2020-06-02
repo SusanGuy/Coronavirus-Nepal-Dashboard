2af78927a112e713e119c7644b77dc065856c181
sushantbaskota2 <sushantbaskota2@gmail.com>
2020-06-01 22:42:57 -0500

    fixed props problem

--- src/containers/Home/home.js
+++ src/containers/Home/home.js
@@ -1,133 +1,63 @@
 import React, { Fragment, useEffect, useState } from "react";
 import _ from "lodash";
-import Footer from "../../components/Footer/Footer";
 import moment from "moment";
 import axios from "../../axios";
-import {
-  handleMerge,
-  calculateAdditional,
-  caclulateTimeSeries,
-  getTotalDiff,
-} from "../../utils";
 import ContentLoader from "../../components/ContentLoader/ContentLoader";
 import RightContainer from "../../components/RightContainer/RightContainer";
 import LeftContainer from "../../components/LeftContainer/LeftContainer";
 const Home = ({ mode }) => {
   const [mainCases, setTotalCases] = useState({
     totalCases: [],
+    loading: true,
   });
 
+  const [selectType, setSelectType] = useState(2);
   const [covidData, setCovidData] = useState([]);
-  const [totalData, setTotalData] = useState(null);
-  const [fetched, setFetched] = useState(false);
 
   useEffect(() => {
     let unmounted = false;
-
-    const getStates = async () => {
+    const getTotalData = async () => {
       try {
-        const [
-          { data: totalData },
-          { data },
-          { data: ownData },
-        ] = await Promise.all([
-          axios.get("/"),
-          axios.get("https://data.nepalcorona.info/api/v1/covid"),
-          axios.get("/chart"),
-        ]);
-
+        const { data } = await axios.get("/");
         if (!unmounted) {
-          setTotalCases({ totalCases: totalData });
-          setCovidData(data);
-          setTotalData(ownData);
-          setFetched(true);
+          setTotalCases({ totalCases: data, loading: false });
         }
-      } catch (err) {
-        console.log(err);
+      } catch (error) {
+        setTotalCases({ totalCases: [], loading: false });
+        console.log(error);
+      }
+    };
+    const getAllFacts = async () => {
+      try {
+        const { data } = await axios.get(
+          "https://data.nepalcorona.info/api/v1/covid"
+        );
+        if (!unmounted) {
+          setCovidData(data);
+        }
+      } catch (error) {
+        console.log(error);
       }
     };
 
-    getStates();
+    getAllFacts();
+    getTotalData();
 
     return () => {
       unmounted = true;
     };
   }, []);
 
-  const { totalCases } = mainCases;
-  let sahiCases = [];
+  const calculateAdditional = (arr, field) => {
+    return arr.reduce((init, current) => init + current[field], 0);
+  };
+
+  const { totalCases, loading } = mainCases;
+
   let districtCases = [];
-  const provinceCases = [];
-  let daiCases = [];
-  let facts = {};
-  let groupedTimeline = {};
-  if (fetched) {
-    sahiCases = totalCases.map((dai) => {
-      const totalCases = covidData.filter((dat) => dat.district === dai.id);
-
-      return {
-        ...dai,
-        ...getTotalDiff(dai.date, totalCases, {
-          total: parseInt(dai.total),
-          deaths: parseInt(dai.deaths),
-          recovered: parseInt(dai.recovered),
-        }),
-      };
-    });
-
-    districtCases = sahiCases.map(
-      ({ date, provinceId, provincename, ...rest }) => {
-        return {
-          ...rest,
-        };
-      }
-    );
-    let heroCases = {};
-    sahiCases.forEach(
-      ({
-        provinceId,
-        name,
-        id,
-        total,
-        deaths,
-        recovered,
-        active,
-        additionalTotal,
-        additionalRecovery,
-        additionalDeaths,
-        additionalActive,
-      }) => {
-        let hero = {
-          name,
-          id,
-          total,
-          deaths,
-          recovered,
-          active,
-          additionalTotal,
-          additionalRecovery,
-          additionalDeaths,
-          additionalActive,
-        };
-        if (heroCases[provinceId]) {
-          heroCases[provinceId].push(hero);
-        } else {
-          heroCases[provinceId] = [hero];
-        }
-      }
-    );
-
-    Object.keys(heroCases).forEach((province) => {
-      daiCases.push({
-        id: parseInt(province),
-        name: totalCases.find(
-          ({ provinceId }) => province.toString() === provinceId.toString()
-        ).provinceName,
-        districts: heroCases[province],
-      });
-    });
-
-    daiCases.forEach(({ id, name, districts }) => {
+  let provinceCases = [];
+  if (totalCases.length !== 0) {
+    totalCases.forEach(({ id, name, districts }) => {
       provinceCases.push({
         id,
         name,
@@ -143,67 +73,142 @@ const Home = ({ mode }) => {
         additionalDeaths: calculateAdditional(districts, "additionalDeaths"),
         additionalActive: calculateAdditional(districts, "additionalActive"),
       });
+      districtCases.push(...districts);
     });
-
-    facts = {
-      total: districtCases.reduce((init, current) => init + current.total, 0),
-      recovered: districtCases.reduce(
-        (init, current) => init + current.recovered,
-        0
-      ),
-      active: districtCases.reduce((init, current) => init + current.active, 0),
-      deaths: districtCases.reduce((init, current) => init + current.deaths, 0),
-      newTotal: districtCases.reduce(
-        (init, current) => init + current.additionalTotal,
-        0
-      ),
-      newRecovered: districtCases.reduce(
-        (init, current) => init + current.additionalRecovery,
-        0
-      ),
-      newDeath: districtCases.reduce(
-        (init, current) => init + current.additionalDeaths,
-        0
-      ),
-      newActive: districtCases.reduce(
-        (init, current) => init + current.additionalActive,
-        0
-      ),
-    };
-
-    groupedTimeline = _.mapValues(
-      _.groupBy(
-        covidData.map((dat) => {
-          return {
-            ...dat,
-            createdOn: moment(new Date(dat.createdOn)).format("YYYY-MM-DD"),
-          };
-        }),
-        "province"
-      ),
-      (clist) => clist.map((data) => _.omit(data, "province"))
-    );
-
-    Object.keys(groupedTimeline).forEach((element) => {
-      const merged = handleMerge(groupedTimeline[element]);
-      groupedTimeline[element] = Object.keys(merged).map((val) => {
-        return {
-          date: new Date(val).setHours(0, 0, 0),
-
-          ...merged[val],
-        };
-      });
-    });
-
-    groupedTimeline = caclulateTimeSeries(groupedTimeline);
   }
 
+  const facts = {
+    total: districtCases.reduce((init, current) => init + current.total, 0),
+    recovered: districtCases.reduce(
+      (init, current) => init + current.recovered,
+      0
+    ),
+    active: districtCases.reduce((init, current) => init + current.active, 0),
+    deaths: districtCases.reduce((init, current) => init + current.deaths, 0),
+    newTotal: districtCases.reduce(
+      (init, current) => init + current.additionalTotal,
+      0
+    ),
+    newRecovered: districtCases.reduce(
+      (init, current) => init + current.additionalRecovery,
+      0
+    ),
+    newDeath: districtCases.reduce(
+      (init, current) => init + current.additionalDeaths,
+      0
+    ),
+    newActive: districtCases.reduce(
+      (init, current) => init + current.additionalActive,
+      0
+    ),
+  };
+
+  let groupedTimeline = _.mapValues(
+    _.groupBy(
+      covidData.map((dat) => {
+        return {
+          ...dat,
+          createdOn: moment(new Date(dat.createdOn)).format("YYYY-MM-DD"),
+        };
+      }),
+      "province"
+    ),
+    (clist) => clist.map((data) => _.omit(data, "province"))
+  );
+
+  const handleMerge = (arr) => {
+    const khali = {};
+    arr.forEach((arr) => {
+      if (khali[arr.createdOn]) {
+        let total = khali[arr.createdOn].dailyconfirmed;
+        let recovered = khali[arr.createdOn].dailyrecovered;
+        let deaths = khali[arr.createdOn].dailydeceased;
+
+        khali[arr.createdOn].dailyconfirmed = total + 1;
+        khali[arr.createdOn].dailyrecovered =
+          arr.recoveredOn !== null ? recovered + 1 : recovered;
+        khali[arr.createdOn].dailydeceased =
+          arr.deathOn !== null ? deaths + 1 : deaths;
+        khali[arr.createdOn].dailyactive = total - recovered - deaths;
+      } else {
+        let total = 1;
+        let recovered = arr.recoveredOn !== null ? 1 : 0;
+        let deaths = arr.deathOn !== null ? 1 : 0;
+        khali[arr.createdOn] = {
+          dailyconfirmed: total,
+          dailyrecovered: recovered,
+          dailydeceased: deaths,
+          dailyactive: total - recovered - deaths,
+        };
+      }
+    });
+    return khali;
+  };
+
+  Object.keys(groupedTimeline).forEach((element) => {
+    const merged = handleMerge(groupedTimeline[element]);
+    groupedTimeline[element] = Object.keys(merged).map((val) => {
+      return {
+        date: new Date(val).setHours(0, 0, 0),
+
+        ...merged[val],
+      };
+    });
+  });
+
+  Object.keys(groupedTimeline).forEach((e) => {
+    const element = groupedTimeline[e];
+    for (var m = moment("2020-01-01"); m.isBefore(moment()); m.add(1, "days")) {
+      const hero = {
+        dailyconfirmed: 0,
+        dailyrecovered: 0,
+        dailydeceased: 0,
+        dailyactive: 0,
+        date: m.toDate().setHours(0, 0, 0),
+      };
+
+      if (!element.find((estoElement) => moment(estoElement.date).isSame(m))) {
+        element.push(hero);
+      }
+    }
+
+    groupedTimeline[e] = element;
+  });
+
+  Object.keys(groupedTimeline).forEach((e) => {
+    let total = 0;
+    let recovered = 0;
+    let deaths = 0;
+    let active = 0;
+    const sortedVai = groupedTimeline[e].sort((a, b) => {
+      if (moment(a.date) < moment(b.date)) {
+        return -1;
+      } else if (moment(a.date) > moment(b.date)) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+    sortedVai.forEach((l, i) => {
+      total = total + l.dailyconfirmed;
+      deaths = deaths + l.dailydeceased;
+      recovered = recovered + l.dailyrecovered;
+      active = active + l.dailyactive;
+      groupedTimeline[e][i]["totalconfirmed"] = total;
+      groupedTimeline[e][i]["totalactive"] = active;
+      groupedTimeline[e][i]["totaldeceased"] = deaths;
+      groupedTimeline[e][i]["totalrecovered"] = recovered;
+    });
+  });
+
+  console.log(facts.date);
+
+
   return (
-    <Fragment>
-      <div className="Home">
-        {!fetched ? (
-          <ContentLoader />
-        ) : (
+    <div className="Home">
+      {loading ? (
+        <ContentLoader />
+      ) : (
           <Fragment>
             <LeftContainer
               {...facts}
@@ -213,13 +218,14 @@ const Home = ({ mode }) => {
                   : covidData[covidData.length - 1].modifiedOn
               }
               provinceCases={provinceCases}
-              totalData={daiCases}
-              ownData={totalData}
+              totalData={totalCases}
             />
             <RightContainer
               {...facts}
-              ownData={totalData}
+              date={covidData[covidData.length - 1].modifiedOn}
               mode={mode}
+              selectType={selectType}
+              setSelectType={setSelectType}
               provinceCases={provinceCases}
               groupedTimeline={groupedTimeline}
               districtCases={districtCases.sort((a, b) => {
@@ -234,10 +240,7 @@ const Home = ({ mode }) => {
             />
           </Fragment>
         )}
-      </div>
-
-      <Footer />
-    </Fragment>
+    </div>
   );
 };
 
