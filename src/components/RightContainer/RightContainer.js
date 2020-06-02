07813b450c22832559d9cdb90a066b60f673deef
sushantbaskota2 <sushantbaskota2@gmail.com>
2020-06-01 22:35:48 -0500

    Added New Map

--- src/components/RightContainer/RightContainer.js
+++ src/components/RightContainer/RightContainer.js
@@ -1,8 +1,16 @@
 import React from "react";
-
+import MapViewer from '../../MapViewer'
 import Dome from "../../Dome";
 import MapView from "../MapView/MapView";
 const RightContainer = ({
+  total,
+  active,
+  recovered,
+  deaths,
+  newTotal,
+  newRecovered,
+  newDeath,
+  date,
   selectType,
   setSelectType,
   districtCases,
@@ -10,18 +18,30 @@ const RightContainer = ({
   mode,
   groupedTimeline,
 }) => {
+  const facts = {
+    total,
+    active,
+    recovered,
+    deaths,
+    newTotal,
+    newRecovered,
+    newDeath, date
+  }
   return (
     <div className="home-right">
-      <MapView
-        selectType={selectType}
-        setSelectType={setSelectType}
-        districtData={districtCases}
-        provinceData={provinceCases}
-        mode={mode}
-      />
+      {//<MapView
+        //   selectType={selectType}
+        //   setSelectType={setSelectType}
+        //   districtData={districtCases}
+        //   provinceData={provinceCases}
+        //   mode={mode}
+        // />
+      }
 
-      <Dome groupedTimeline={groupedTimeline}></Dome>
-    </div>
+      {facts && <MapViewer dist={districtCases} {...facts}></MapViewer>}
+
+      <Dome facts={facts} date={date} groupedTimeline={groupedTimeline}></Dome>
+    </div >
   );
 };
 
